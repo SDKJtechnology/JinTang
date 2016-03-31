d46e9d57139837703c561f4e2e59c3c4303c7aeb
SDKJtechnology <386182389@qq.com>
2016-03-31 10:12:56 +0800

    3.31 10:12

--- 金堂/DynamicHomeController.m
+++ 金堂/DynamicHomeController.m
@@ -8,8 +8,13 @@
 
 #import "DynamicHomeController.h"
 #import "HTHorizontalSelectionList.h"
+#import <UIView+SDAutoLayout.h>
 
-@interface DynamicHomeController ()<HTHorizontalSelectionListDataSource,HTHorizontalSelectionListDelegate>
+#define VIEW_WIDTH self.view.frame.size.width
+#define VIEW_HEIGHT self.view.frame.size.height
+
+@interface DynamicHomeController ()<HTHorizontalSelectionListDataSource,HTHorizontalSelectionListDelegate,
+UITableViewDelegate,UITableViewDataSource>
 {
     NSArray *selectListData;//水平选择列表数据
 }
@@ -26,6 +31,27 @@
 
     selectListData = @[@"关注",@"热点",@"活动"];
     
+    self.selectList.frame = CGRectMake(0, 64, VIEW_WIDTH, 30);
+    
+    self.contentView.frame = CGRectMake(0, self.selectList.frame.size.height + 64, VIEW_WIDTH, VIEW_HEIGHT - - self.contentView.frame.origin.y - 49 );
+    
+        //  添加TableView
+        CGFloat w = self.contentView.frame.size.width;
+        CGFloat h = self.contentView.frame.size.height;
+    for (NSInteger i = 0; i < selectListData.count; i++) {     /* 有多View取决于categories的数量 */
+            CGRect frame = self.contentView.bounds;
+        frame.origin.x = i * VIEW_WIDTH;
+            UITableView *tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStyleGrouped];
+            tableView.delegate = self;
+            tableView.dataSource = self;
+            tableView.tag = i + 100;
+            [self.contentView addSubview:tableView];
+        }
+        
+        //  设置scrollView
+    self.contentView.contentSize = CGSizeMake(selectListData.count * w, h);
+    self.contentView.pagingEnabled = YES;
+    
     [self.selectList setSelectionIndicatorColor:[UIColor blackColor]];
     [self.selectList setTitleFont:[UIFont fontWithName:@"Arial-BoldItalicMT" size:16] forState:UIControlStateNormal];
     [self.selectList setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
@@ -33,6 +59,8 @@
     self.selectList.delegate = self;
     self.selectList.dataSource = self;
 }
+
+#pragma mark HTHorizontalSelectionListDataSource
 
 - (NSInteger)numberOfItemsInSelectionList:(HTHorizontalSelectionList *)selectionList
 {
@@ -44,9 +72,50 @@
     return selectListData[index];
 }
 
+#pragma mark HTHorizontalSelectionListDelegate
+
 - (void)selectionList:(HTHorizontalSelectionList *)selectionList didSelectButtonWithIndex:(NSInteger)index
 {
-    
+    //  实现scrollView滑动绑定的方法
+    CGRect frame = self.contentView.bounds;
+    frame.origin.x = frame.size.width * index;
+    [self.contentView scrollRectToVisible:frame animated:YES];
+}
+
+#pragma mark UITableViewDataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    cell.textLabel.text = @"12";
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+#pragma mark UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 30;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section;
+{
+    return 10;
 }
 
 @end
