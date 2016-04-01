c88728d6613d20df3ca3b0b9fe3bf911ea53c0c7
SDKJtechnology <zouxin@zouxindeMacintosh.local>
2016-04-01 20:00:15 +0800

    6565

--- 金堂/AppDelegate.m
+++ 金堂/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"212512251");
     return YES;
 }
 
